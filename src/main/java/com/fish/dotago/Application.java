1ce5d03898a2d137f9e3123d0178ad1d695cc872
gongwei <gongwei1993625@163.com>
2017-12-12 09:44:44 +0800

    使用阿里规范扫描，增加了类注解

--- src/main/java/com/fish/dotago/Application.java
+++ src/main/java/com/fish/dotago/Application.java
@@ -3,6 +3,12 @@ package com.fish.dotago;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Wayne
+ * @date 2017年12月12日
+ *
+ * 项目入口
+ */
 @SpringBootApplication
 public class Application {
 
